b6c655ca34803cdce1c2234647f1bdcba29a1dc9
jmas <jmas.ukraine@gmail.com>
2016-09-20 20:29:41 +0300

    Init

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Create new HTMLElement. By default is DIV.
+ * @param {object} attrs Element attributes
+ * @param {*} children Element childrend (HTMLElement, array of HTMLElements, string, HTML string, array of strings)
+ * @param {string} tag Element tag. By default is DIV
+ * @returns {HTMLElement}
+ */
+export default function el (attrs={}, children=null, tag='div') {
+  let _el = document.createElement(tag);
+  for (let attrName in attrs) {
+    if (attrs.hasOwnProperty(attrName)) {
+      _el[attrName] = (attrName.indexOf('on')===0 ? attrs[attrName].bind(null, _el): attrs[attrName]);
+    }
+  }
+  if (children) {
+    if (children instanceof Array) {
+      for (let i=0,ln=children.length; i<ln; i++) {
+        if (typeof children[i]==='array' || typeof children[i]==='string') {
+          _el.appendChild(el({}, children[i]));
+        } else {
+          _el.appendChild(children[i]);
+        }
+      }
+    } else if (typeof children==='object' && children.hasOwnProperty('nodeType')) {
+      _el.appendChild(children);
+    } else {
+      _el.innerHTML = children;
+    }
+  }
+  return _el;
+}
