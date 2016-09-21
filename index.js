fe7cdb4bc46e5dff824bd54d5866b763cd21cb4f
jmas <jmas.ukraine@gmail.com>
2016-09-21 21:49:27 +0300

    Add apply function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @param {string} tag Element tag. By default is DIV
  * @returns {HTMLElement}
  */
-export default function el (children=null, attrs={}, tag='div') {
+function el (children=null, attrs={}, tag='div') {
   let _el = document.createElement(tag);
   for (let attrName in attrs) {
     if (attrs.hasOwnProperty(attrName)) {
@@ -21,11 +21,52 @@ export default function el (children=null, attrs={}, tag='div') {
           _el.appendChild(children[i]);
         }
       }
-    } else if (typeof children==='object' && children.hasOwnProperty('nodeType')) {
-      _el.appendChild(children);
-    } else {
+    } else if (typeof children==='string') {
       _el.innerHTML = children;
+    } else {
+      _el.appendChild(children);
     }
   }
   return _el;
 }
+
+/**
+ * Apply a object of {selector: {function|string|HTMLElement}} to target element.
+ * This function is searching for selector and trying to append value to found element.
+ * If value is a function it will be called with argument that contain element that found by selector.
+ * If value is a string it will be just inserted as a HTML content.
+ * If value is an HTMLElement it will be appended as a child element.
+ * @param {HTMLElement} An target element
+ * @param {object} An object that contain {selector: {function|string|HTMLElement}}
+ * @returns {HTMLElement}
+ */
+function apply (el, children) {
+  if (children instanceof Array) {
+    for (let i=0,ln=children.length; i<ln; i++) {
+      apply(el, children[i]);
+    }
+    return el;
+  }
+  for (let key in children) {
+    if (children.hasOwnProperty(key)) {
+      let placeholderEl = el.querySelector(key);
+      if (placeholderEl) {
+        if (!children[key]) {
+          throw new Error(`The right type of object property value is: 'function', 'string' or 'HTMLElement'. Got '${typeof children[key]}'.`);
+        }
+        if (typeof children[key]==='function') {
+          children[key](placeholderEl);
+        } else if (typeof children[key]==='string') {
+          placeholderEl.innerHTML = children[key];
+        } else {
+          placeholderEl.appendChild(children[key]);
+        }
+      } else {
+        throw new Error(`Selector '${key}' is not found.`);
+      }
+    }
+  }
+  return el;
+}
+
+module.exports = {el, apply};
